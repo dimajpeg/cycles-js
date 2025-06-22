dd2904d4b1809d4ed9bc340b5777cd30e424c637
dima <>
2025-06-22 23:02:04 +0300

    Add age and number input forms with validation and styling

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-const formAge = document.getElementById('form-age')
-const inputAge = document.getElementById('input-age')
+{ // Age
+  const formAge = document.getElementById('form-age')
+  const inputAge = document.getElementById('input-age')
+  const resultOutput = document.getElementById('result-output')
 
-formAge.addEventListener('submit', (e) => {
-  e.preventDefault();
+  formAge.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  const age = Number(inputAge.value)
+    const age = Number(inputAge.value)
 
-  if (isNaN(age) || age < 0) {
-    console.log('Будь ласка, введіть коректний вік (додатнє число).')
-  } else {
-    if (age <= 11) {
-      console.log('вы ребенок')
+    let message = ''
+
+    if (isNaN(age) || age <= 0) {
+      message = 'Будь ласка, введіть коректний вік (додатнє число).'
+    } else {
+      if (age <= 11) {
+        message = 'Ви дитина.'
+      } else if (age <= 17) {
+        message = 'Ви підліток.'
+      } else if (age <= 59) {
+        message = 'Ви дорослий.'
+      } else if (age >= 60) {
+        message = 'Ви пенсіонер.'
+      }
     }
-    else if (age <= 17) {
-      console.log('вы подросток')
-    }
-    else if (age <= 59) {
-      console.log('вы взрослый')
-    }
-    else if (age >= 60) {
-      console.log('вы пенсионер')
-    }
-  }
-})
+
+    resultOutput.textContent = message
+
+  })
+}
+
+{
+  const numberForm = document.getElementById('number-form')
+  const numberResult = document.getElementById('number-result')
+
+  const char = ")!@#$%^&*("
+
+  let message = ''
+
+  numberForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const i = numberForm.number.value
+
+    message = char[i]
+
+    numberResult.textContent = message || 'Будь ласка, введіть коректне число (0-9).'
+  })
+
+}
+
+
